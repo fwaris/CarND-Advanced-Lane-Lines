c03b308b525d74b7d57264e3603be7fa3578d4c6
Faisal Waris <faisalwaris@live.com>
2017-08-27 15:27:22 -0400

    curvature change

--- AdvancedLaneMarking/LineFitting.fs
+++ AdvancedLaneMarking/LineFitting.fs
@@ -23,18 +23,34 @@ let fitLine (pts:Point seq) =
     let c,b,a = ps.[0],ps.[1],ps.[2]
     QuadTrns (c,b,a)
 
-let ym_per_pix = 30./720. //meters per pixel in y dimension
-let xm_per_pix = 3.7/700. //meters per pixel in x dimension
+let Hm_per_pix = 30./720. //meters per pixel in y dimension
+let Wm_per_pix = 3.7/700. //meters per pixel in x dimension
 
-let toWorldLine sz line =
-    linePoints sz line
-    |> List.map (fun p -> float p.X * xm_per_pix, float p.Y * ym_per_pix)
-    |> List.map Point
-    |> fitLine
+let toWorldLine sz (QuadTrns(c,b,a)) =
+    let worldPts = 
+        [for i in  0..sz -> let x = float i in x,c + b * x + a * (x**2.)]
+        |> List.map (fun (tX,tY) -> (float sz) - tX, tY) //from top down to bottom up
+        |> List.map (fun (tX,tY) -> tX * Hm_per_pix, tY * Wm_per_pix) //x y are swapped 
+        |> List.map Point
+    let xs,ys = splitPoints worldPts
+    let m =  LinearRegression.DirectRegressionMethod.NormalEquations
+    let ps = Fit.Polynomial(xs,ys,2,m)
+    let c,b,a = ps.[0],ps.[1],ps.[2]
+    QuadTrns (c,b,a)
 
-let curvature y (QuadTrns(c,b,a)) = 
-    let y = float y
-    let x = c + b * y + a * (y**2.)
+let curvature maxH (QuadTrns(c,b,a)) = 
+    let x = float maxH
+    let y = c //+ b * x + a * (x**2.)
+    //y_eval = np.max(ploty)
+    let c = ((1. + (2.*a*0. + b)**2.)**1.5) / abs (2.0 * a)
+    c
+    //left_curverad = ((1 + (2*left_fit[0]*y_eval + left_fit[1])**2)**1.5) / np.absolute(2*left_fit[0])
+    //right_curverad = ((1 + (2*right_fit[0]*y_eval + right_fit[1])**2)**1.5) / np.absolute(2*right_fit[0])
+    //print(left_curverad, right_curverad)
+
+let curvatureOld maxH (QuadTrns(c,b,a)) = 
+    let x = float maxH
+    let y = c + b * x + a * (x**2.)
     //y_eval = np.max(ploty)
     let c = ((1. + (2.*a*x + b)**2.)**1.5) / abs (2.0 * a)
     c
